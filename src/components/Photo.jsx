47a272d3a10f81469020619e48b84f3a2839a27b
Filip <dev.filip0s@protonmail.com>
2022-07-07 11:32:55 +0200

    Imported profile photo to the `Photo` component
    Also added alt text to the photo

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,5 +1,8 @@
+import profilePhoto from "../images/bateman.webp"
+
+
 export default function Photo() {
     return (
-        <img className="card--profile-photo" alt="" src=""/>
+        <img className="card--profile-photo" alt="Profile photo of Patrick Bateman" src={profilePhoto}/>
     )
 }
