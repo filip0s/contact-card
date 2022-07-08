369ca2dd4165d153711c0d8e9e9e0e87184965fa
Filip <dev.filip0s@protonmail.com>
2022-07-08 15:14:25 +0200

    Wrapper components of `Interests` and `About` components were changed to be `<section>` tags
    Tags changed to `<section>` so they make better use of html5 semantic tags and also are easily targeted for styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,8 @@
 export default function About(){
     return (
-        <div className="card--about">
+        <section className="card--about">
             <h2>About</h2>
             <p>about me...</p>
-        </div>
+        </section>
     )
 }
--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -1,8 +1,8 @@
 export default function Interests(){
     return (
-        <div className="card--interests">
+        <section className="card--interests">
             <h2>Interests</h2>
             <p>my interests...</p>
-        </div>
+        </section>
     )
 }
