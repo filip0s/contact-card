a1d3bee7d2e5f14bb19663769fd38c8d7daa8de0
Filip <dev.filip0s@protonmail.com>
2022-07-09 17:12:41 +0200

    Added content into `Footer` component
    Imported and rendered icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
-export default function Footer() {
-    return(
-        <footer>
+import twitterIcon from "../icons/Twitter Icon.svg"
+import linkedinIcon from "../icons/Linkedin Icon.svg"
+import instagramIcon from "../icons/Instagram Icon.svg"
+import githubIcon from "../icons/GitHub Icon.svg"
+import facebookIcon from "../icons/Facebook Icon.svg"
 
+export default function Footer() {
+    return (
+        <footer>
+            <a href="#"><img src={twitterIcon} alt="Twitter icon"/></a>
+            <a href="#"><img src={facebookIcon} alt="Facebook icon"/></a>
+            <a href="#"><img src={instagramIcon} alt="Instagram icon"/></a>
+            <a href="#"><img src={linkedinIcon} alt="LinkedIn icon"/></a>
+            <a href="#"><img src={githubIcon} alt="GitHub icon"/></a>
         </footer>
     )
 }
